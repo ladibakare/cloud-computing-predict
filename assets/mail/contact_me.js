665797fd76225a78324a50a4d3f4914326ff71ca
ladibakare <99255933+ladibakare@users.noreply.github.com>
2023-07-12 23:57:10 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://f94vfathm1.execute-api.eu-west-1.amazonaws.com/default/Cloud-Computing",
+                url: "https://f94vfathm1.execute-api.eu-west-1.amazonaws.com/default",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
